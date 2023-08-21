8c591f99758c4240b6c905646ec5e05c668feda3
Ivan Coronado <ivan.cduran@gmail.com>
2023-08-21 15:19:16 -0600

    hero shoes

--- src/lib/shoeCard.svelte
+++ src/lib/shoeCard.svelte
@@ -4,21 +4,17 @@
 	export let bigShoeImg;
 
 	const handleClick = () => {
-		console.log('click');
-		if (bigShoeImg !== imgURL.bigShoe) {
-			changeBigShoeImage(imgURL.bigShoe);
-		}
+		changeBigShoeImage = imgURL.bigShoe;
 	};
 </script>
 
 <div
-	class={`border-2 rounded-lg
-		${bigShoeImg === imgURL.bigShoe ? 'border-color-red' : 'border-transparent'}
-		cursor-pointer max-sm:flex-1`}
+	class="border-2 rounded-lg cursor-pointer max-sm:flex-1"
+	class:border-red-500={imgURL.bigShoe == bigShoeImg}
 	on:click={handleClick}
 >
 	<div
-		class="flex justify-center items-center bg-card bg-center bg-cover sm:w-40 sm:h-40 rounded-lg max-sm:p-4 border-2"
+		class="flex justify-center items-center bg-card bg-center bg-cover sm:w-40 sm:h-40 rounded-lg max-sm:p-4"
 	>
 		<img
 			src={imgURL.thumbnail}
